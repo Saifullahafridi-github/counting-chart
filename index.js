b90d80cb307947152208c90fe2152cab2671a608
saifullahafridi-github <saifullahafridi077@gmail.com>
2023-07-30 02:26:07 +0500

    daynamic chart

--- index.js
+++ index.js
@@ -8,3 +8,24 @@ function previous(){
     num--;
     doc.innerHTML=num
 }
+function Reset(){
+    num= 0;
+    doc.innerHTML=num 
+}
+
+let tab=2;
+function table2(){
+    document.getElementById('head3') .innerHTML=""
+    for(let cou=1;cou<11;cou++){
+        document.getElementById('head3').innerHTML+= tab+'x'+ cou+'='+tab*cou+ "<br>"
+    }
+}
+
+function daynamic(isdp){
+document.getElementById('head2') .innerHTML=""
+for(let cou=1;cou<11;cou++){
+    document.getElementById('head2').innerHTML+= isdp+'x'+ cou+'='+isdp*cou+ "<br>"
+}
+}
+
+
